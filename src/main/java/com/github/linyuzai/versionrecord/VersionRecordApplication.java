0da0e86d7ac3daafc870062a93ba41ad8b542e7e
linyuzai <120718461@qq.com>
2020-02-28 14:18:52 +0800

    Initial commit

---
+++ src/main/java/com/github/linyuzai/versionrecord/VersionRecordApplication.java
@@ -0,0 +1,13 @@
+package com.github.linyuzai.versionrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/linyuzai/versionrecord/VersionRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.linyuzai.versionrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
